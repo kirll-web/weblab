3ea7c50a79cfe9c9dd54d497299e8a0cd6de0b78
Kirill Dikov <reghansan@gmail.com>
2023-05-06 17:20:00 +0300

    update style admin-form, update class of button in admin-login. Create js

--- static/script/index.js
+++ static/script/index.js
@@ -1,10 +1,36 @@
 'use strict';
 
-// document.addEventListener('DOMContentLoaded', () => {
-//     const UploadPhoto = () => {
-//         const UploadBtn = document.querySelector('#'),;
-//     };
+document.addEventListener('DOMContentLoaded', () => {
+    
+    const postForm = document.querySelector('#admin-form');
 
-//     UploadPhoto();
+    const validateInputText = (inputClass) => {
+        const inputText = document.querySelectorAll(inputClass);
+        console.log(inputText.value);
+        for (let i = 0; i < inputText.length; i++) {
+            if (inputText[i].value == "" || inputText[i].value == undefined || inputText[i].value == null ) {
+                console.log(i);
+                return false;
+            }
+            console.log(inputText.value);
+        }
+        return true;
+    };
 
-// });
+    postForm.addEventListener('submit', (event) => {
+        event.preventDefault();
+        const formData = new FormData(postForm);
+
+        
+        if (validateInputText('.admin-form__input-text')) {
+            console.log(JSON.stringify(Object.fromEntries(formData)));
+        } else {
+            console.error('Ошибка ввода данных');
+        }
+
+    });
+    for (let i = 0; i++; i < 5) {
+        console.log('Я');
+    }
+
+});
