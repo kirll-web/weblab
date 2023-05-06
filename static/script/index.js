79fc6ab7033731e423f4c1954cf6758069e8cb64
Kirill Dikov <reghansan@gmail.com>
2023-05-06 17:22:03 +0300

    small update js

--- static/script/index.js
+++ static/script/index.js
@@ -4,17 +4,20 @@ document.addEventListener('DOMContentLoaded', () => {
     
     const postForm = document.querySelector('#admin-form');
 
-    const validateInputText = (inputClass) => {
-        const inputText = document.querySelectorAll(inputClass);
-        console.log(inputText.value);
-        for (let i = 0; i < inputText.length; i++) {
-            if (inputText[i].value == "" || inputText[i].value == undefined || inputText[i].value == null ) {
-                console.log(i);
-                return false;
-            }
+    const validateForm = () => {
+        const validateInputText = (inputClass) => {
+            const inputText = document.querySelectorAll(inputClass);
             console.log(inputText.value);
-        }
-        return true;
+            for (let i = 0; i < inputText.length; i++) {
+                if (inputText[i].value == "" || inputText[i].value == undefined || inputText[i].value == null ) {
+                    console.log(i);
+                    return false;
+                }
+                console.log(inputText.value);
+            }
+            return true;
+        };
+        validateInputText('.admin-form__input-text');
     };
 
     postForm.addEventListener('submit', (event) => {
@@ -22,7 +25,7 @@ document.addEventListener('DOMContentLoaded', () => {
         const formData = new FormData(postForm);
 
         
-        if (validateInputText('.admin-form__input-text')) {
+        if () {
             console.log(JSON.stringify(Object.fromEntries(formData)));
         } else {
             console.error('Ошибка ввода данных');
